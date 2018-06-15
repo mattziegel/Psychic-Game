f9a04c329f77916b613b29911a4af18eb62fb233
Lefty Lang <kitchensink@Kitchens-MacBook-Pro.local>
2018-06-14 22:30:56 -0400

    Final Commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Psychic-Game
+Can you guess what letter Therman is thinking of? 
+
+In this game you have 9 guesses to figure out which letter Therman is thinking of. If you guess correct in under 9 guesses you gain a win!!! and the game resets. If you run out of guesses before selecting the correct letter you gain a loss :( and the game resets. Play as much as you like!   
+
+Therman is a character in the movie Dinner for Schmucks played by Zack Galifianakis, displayed here in the page's background image, he is a self described master of mind control. Whether you win or lose, Therman will have something to say about it. Challenge him at your peril.
+
+Background music by Doug Maxwell, "Space Chatter"
---
+++ assets/javascript/game.js
@@ -0,0 +1,45 @@
+var letters = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
+
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessLetters = [];
+
+document.onkeydown = function (event) {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+    guessLetters.push(userGuess);
+    
+    if (userGuess === computerGuess) {
+        wins++;
+        guesses = 9;
+        guessLetters.length = 0;
+        alert("You have bested me, I release you!")
+    } 
+     
+    else if (guesses == 0) {
+        losses++;
+        guessLetters.length = 0;
+        guesses = 9;
+        alert("Ha! You Lose!")
+    }
+
+    else if (userGuess !== computerGuess) {
+        guesses--;
+    }
+
+    // else if (userGuess !== letters.length){
+    //     alert("What?! That's not a letter!");
+    // }
+
+        
+    var html =
+        "<p>Wins: " + wins + "</p>" +
+        "<p>Loses: " + losses + "</p>" +
+        "<p>Guesses Left: " + guesses + "<p>" +
+        "<p>Your Guesses So Far: " + guessLetters + "<p>";
+
+    document.querySelector("#game").innerHTML = html;
+
+}
+
